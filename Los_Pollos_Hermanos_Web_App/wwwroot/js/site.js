d76b931110ca0b3b55e561306919a218d7b251fb
will <allwood.w2003@gmail.com>
2023-01-18 18:48:47 +0000

    Changes to CSS alongside contact form, searchbar  and changes to the index page

--- Los_Pollos_Hermanos_Web_App/wwwroot/js/site.js
+++ Los_Pollos_Hermanos_Web_App/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+<script>
+    function myFunction() {
+  var input, filter, ul, li, a, i;
+    input = document.getElementById("mySearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myMenu");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+    } else {
+        li[i].style.display = "none";
+    }
+  }
+}
+</script>
